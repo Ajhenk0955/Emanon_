3b29c4afbf41844e372c898ff486a685e6ad7328
AHenk <ajhenk0955@eagle.fgcu.edu>
2015-03-27 18:53:02 -0400

    Some Void Methods
    Creating Stubs

--- src/backdoor_/DataBase.java
+++ src/backdoor_/DataBase.java
@@ -8,4 +8,44 @@ package backdoor_;
  */
 public class DataBase {
 
+	/**
+	 * Uploads data from Patient objects
+	 */
+	private void UpdateData(Patient[] Patients) {
+		// TODO
+	}
+
+	/**
+	 * Fetches Patient Info from database
+	 * 
+	 * @param UID
+	 * @return Patient Files
+	 */
+	private Patient PullPatientProfile(int UID) {
+		return null;
+		// TODO
+	}
+
+	/**
+	 * Returns list of most Recent Patients
+	 */
+	private void GetMostRecent() {
+		// TODO
+	}
+
+	/**
+	 * Returns list of Patients covered by insurer
+	 */
+	private void GetInsurer(String Insurer) {
+		// TODO
+	}
+
+	/**
+	 * returns list of patients with selected part replacement
+	 * 
+	 * @param Part
+	 */
+	private void GetPart(String Part) {
+		// TODO
+	}
 }
--- src/backdoor_/Patient.java
+++ src/backdoor_/Patient.java
@@ -2,9 +2,102 @@ package backdoor_;
 
 /**
  * Container for each patient's information
+ * 
  * @author Andrew
- *
+ * 
+ *         TODO potentially encrypt all info here?
  */
 public class Patient {
-	
+	// ID
+	private String UID;
+
+	// Name
+	private String FName;
+	private String MName;
+	private String LName;
+
+	// DOB
+	private int Day;
+	private int Month;
+	private int Year;
+
+	// Insurance type
+	private String PrimaryInsurance;
+	private String SecondaryInsurance;
+
+	// Social security
+	private String SSN;
+
+	public String getFName() {
+		return FName;
+	}
+
+	public void setFName(String fName) {
+		FName = fName;
+	}
+
+	public String getMName() {
+		return MName;
+	}
+
+	public void setMName(String mName) {
+		MName = mName;
+	}
+
+	public String getLName() {
+		return LName;
+	}
+
+	public void setLName(String lName) {
+		LName = lName;
+	}
+
+	public int getDay() {
+		return Day;
+	}
+
+	public void setDay(int day) {
+		Day = day;
+	}
+
+	public int getMonth() {
+		return Month;
+	}
+
+	public void setMonth(int month) {
+		Month = month;
+	}
+
+	public int getYear() {
+		return Year;
+	}
+
+	public void setYear(int year) {
+		Year = year;
+	}
+
+	public String getPrimaryInsurance() {
+		return PrimaryInsurance;
+	}
+
+	public void setPrimaryInsurance(String primaryInsurance) {
+		PrimaryInsurance = primaryInsurance;
+	}
+
+	public String getSecondaryInsurance() {
+		return SecondaryInsurance;
+	}
+
+	public void setSecondaryInsurance(String secondaryInsurance) {
+		SecondaryInsurance = secondaryInsurance;
+	}
+
+	public String getSSN() {
+		return SSN;
+	}
+
+	public void setSSN(String sSN) {
+		SSN = sSN;
+	}
+
 }
