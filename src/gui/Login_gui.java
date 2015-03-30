18364a15515aac73ff90e39c8c4a389b19b010e4
sjramirez7801 <ramirezsammy10@gmail.com>
2015-03-30 18:51:20 -0400

    login gui
    the window to login

--- src/gui/Login_gui.java
+++ src/gui/Login_gui.java
@@ -6,10 +6,16 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
 
 public class Login_gui extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JLabel lblPassword;
 
 	/**
 	 * Launch the application.
@@ -35,8 +41,29 @@ public class Login_gui extends JFrame {
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JButton btnNewButton = new JButton("Login ");
+		btnNewButton.setBounds(241, 211, 183, 23);
+		contentPane.add(btnNewButton);
+		
+		textField = new JTextField();
+		textField.setBounds(183, 89, 144, 20);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setColumns(10);
+		textField_1.setBounds(183, 129, 144, 20);
+		contentPane.add(textField_1);
+		
+		JLabel lblUserName = new JLabel("Username");
+		lblUserName.setBounds(122, 89, 60, 20);
+		contentPane.add(lblUserName);
+		
+		lblPassword = new JLabel("Password");
+		lblPassword.setBounds(122, 129, 60, 20);
+		contentPane.add(lblPassword);
 	}
-
 }
