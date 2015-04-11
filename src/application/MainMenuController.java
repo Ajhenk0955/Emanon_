b864a18208333523f8996646cce7e463e0a74aec
Actilaurel <gaticomoni@gmail.com>
2015-04-10 22:34:35 -0700

    yum yum

--- src/application/MainMenuController.java
+++ src/application/MainMenuController.java
@@ -1,12 +1,21 @@
 package application;
 
+import javafx.event.ActionEvent;
 import java.net.URL;
 import java.util.ResourceBundle;
-
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 
-public class MainMenuController {
+public class MainMenuController implements Initializable {
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
 		
+	}
+	
+	private void buttonClick1(ActionEvent click1){
+		
+	}
 }
