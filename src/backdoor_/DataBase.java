724c5cdf5be1b3dc71f95475fa86eaff026aa381
AHenk <ajhenk0955@eagle.fgcu.edu>
2015-04-10 22:17:43 -0400

    Maven Support
    And some mongo stuff :|

--- src/backdoor_/DataBase.java
+++ src/backdoor_/DataBase.java
@@ -1,5 +1,12 @@
 package backdoor_;
 
+import org.bson.Document;
+
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
+import com.mongodb.client.MongoDatabase;
+
 /**
  * contains methods for pulling and pushing from database
  * 
@@ -8,10 +15,15 @@ package backdoor_;
  */
 public class DataBase {
 	
+	private MongoClient mongoClient;
+	private MongoDatabase database;
+	private MongoCollection<Document> collection;
+	
 	public DataBase(){
-		
-		MongoClient mongoClient = new MongoClient( "localhost" );
-		MongoDatabase database = mongoClient.getDatabase("mydb");
+		//TODO finish implementing mongo
+		mongoClient = new MongoClient( "localhost" );
+		database = mongoClient.getDatabase("patientDataBase");
+		collection = database.getCollection("test");
 	}
 
 	/**
@@ -30,6 +42,8 @@ public class DataBase {
 	private Patient PullPatientProfile(int UID) {
 		return null;
 		// TODO
+		Document myDoc = collection.find(eq("i", 71)).first();
+		System.out.println(myDoc.toJson());
 	}
 
 	/**
@@ -62,5 +76,32 @@ public class DataBase {
 	 */
 	private void GetName(String SearchTerm) {
 		// TODO
+	}
+	
+	private void GetAllPatients(){
+		MongoCursor<Document> cursor = collection.find().iterator();
+		try {
+		    while (cursor.hasNext()) {
+		        System.out.println(cursor.next().toJson());
+		    }
+		} finally {
+		    cursor.close();
+		}
+	}
+
+	public MongoClient getMongoClient() {
+		return mongoClient;
+	}
+
+	public void setMongoClient(MongoClient mongoClient) {
+		this.mongoClient = mongoClient;
+	}
+
+	public MongoDatabase getDatabase() {
+		return database;
+	}
+
+	public void setDatabase(MongoDatabase database) {
+		this.database = database;
 	}
 }
