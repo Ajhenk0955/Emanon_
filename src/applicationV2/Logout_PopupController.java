8864a1dbbb5e21ee0c41eddd8c53ab9bdfae9475
Actilaurel <gaticomoni@gmail.com>
2015-04-18 00:33:23 -0700

    bridges II

---
+++ src/applicationV2/Logout_PopupController.java
@@ -0,0 +1,32 @@
+package applicationV2;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class Logout_PopupController implements Initializable {
+
+@FXML
+private Button logout_Cancel;
+@FXML
+private Button logout_Yes;
+
+@FXML//TODO
+private void handleLogoutPopup(ActionEvent e){
+	//Stage stage;
+	//Parent root;
+}	
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
