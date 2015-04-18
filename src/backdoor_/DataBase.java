34b3d724d9b11e1ee02fde27cdc98d4c9ec08600
AHenk <ajhenk0955@eagle.fgcu.edu>
2015-04-17 20:32:23 -0400

    Database Stuff
    potato

--- src/backdoor_/DataBase.java
+++ src/backdoor_/DataBase.java
@@ -1,5 +1,27 @@
 package backdoor_;
 
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+import org.bson.Document;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.BulkWriteOperation;
+import com.mongodb.BulkWriteResult;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientOptions;
+import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
+import com.mongodb.client.MongoDatabase;
+
 /**
  * contains methods for pulling and pushing from database
  * 
@@ -8,22 +30,51 @@ package backdoor_;
  */
 public class DataBase {
 
+	private MongoClient mongoClient;
+	private DB database;
+	private DBCollection collection;
+
+	/**
+	 * sets up connection and collection TODO make close connection on end.
+	 * TODO FIX TO NON-DEPRICATED METHOD
+	 * @param userName
+	 * @param databaseName
+	 * @param password
+	 */
+	public DataBase(String userName, String databaseName, char[] password) {
+		MongoCredential credential = MongoCredential.createCredential(userName,
+				databaseName, password);
+		MongoClient mongoClient = new MongoClient(
+				new ServerAddress("localHost"), Arrays.asList(credential));
+		database = mongoClient.getDB(databaseName);
+
+		collection = database.getCollection("users");
+
+	}
+
 	/**
 	 * Uploads data from Patient objects
 	 */
-	private void UpdateData(Patient[] Patients) {
-		// TODO
+	private void UpdateData(DBObject[] Patients) {
+		// TODO test to make sure this works!
+		collection.insert(Patients);
 	}
 
 	/**
 	 * Fetches Patient Info from database
 	 * 
 	 * @param UID
+	 * @return 
 	 * @return Patient Files
 	 */
-	private Patient PullPatientProfile(int UID) {
-		return null;
+	private  DBObject PullPatientProfile(int UID) {
+		BasicDBObject temp = new BasicDBObject("_ID", UID);
 		// TODO
+		DBCursor cursor = collection.find(temp);
+		DBObject temp1 = cursor.next();
+		cursor.close();
+		return temp1;
+
 	}
 
 	/**
@@ -56,5 +107,32 @@ public class DataBase {
 	 */
 	private void GetName(String SearchTerm) {
 		// TODO
+	}
+
+	private void GetAllPatients() {
+		MongoCursor<Document> cursor = collection.find().iterator();
+		try {
+			while (cursor.hasNext()) {
+				System.out.println(cursor.next().toJson());
+			}
+		} finally {
+			cursor.close();
+		}
+	}
+
+	public MongoClient getMongoClient() {
+		return mongoClient;
+	}
+
+	public void setMongoClient(MongoClient mongoClient) {
+		this.mongoClient = mongoClient;
+	}
+
+	public MongoDatabase getDatabase() {
+		return database;
+	}
+
+	public void setDatabase(MongoDatabase database) {
+		this.database = database;
 	}
 }
