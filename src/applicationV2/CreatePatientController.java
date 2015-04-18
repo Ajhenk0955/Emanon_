6eb88e1344f14c15dee0bd33c16b00bb7657b1a0
Actilaurel <gaticomoni@gmail.com>
2015-04-18 01:14:52 -0700

    bridges III
    linked the "Add patient" to the "Patient Profile"

--- src/applicationV2/CreatePatientController.java
+++ src/applicationV2/CreatePatientController.java
@@ -1,13 +1,49 @@
 package applicationV2;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 public class CreatePatientController implements Initializable {
 
+	@FXML
+	private Button saveButton;
 	
+	/** 
+	 * Method for when user clicks the save button in CreatePatient
+	 * @Author M
+	 */
+	
+	@FXML
+	private void handleSaveButton(ActionEvent click) throws IOException {
+		Stage stage;
+		Parent root;
+
+		// User clicks save button
+		if (click.getSource() == saveButton) {
+
+			// finding reference for button's stage
+			stage = (Stage) saveButton.getScene().getWindow();
+			// now loading PatientProfile as parent
+			root = FXMLLoader.load(getClass().getResource(
+					"/applicationV2/PatientProfile.fxml"));
+			
+			// makes PatientProfile scene and show it on the stage
+			Scene patientProfile = new Scene(root);
+			stage.setScene(patientProfile);
+			stage.setTitle("Patient Profile");
+			stage.show();
+		}
+	}
 	
 	
 	public void initialize(URL arg0, ResourceBundle arg1) {
---
+++ src/applicationV2/PatientProfileController.java
@@ -0,0 +1,24 @@
+package applicationV2;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+
+public class PatientProfileController implements Initializable{
+
+	
+	
+	
+	
+	
+	
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
