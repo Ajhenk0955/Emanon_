76c67b15c98ae0c626e92939d48b4537a457c239
Actilaurel <gaticomoni@gmail.com>
2015-04-19 15:47:18 -0700

    bridges VIII
    added the connection to the patient profile and a way back to the main
    menu

--- src/applicationV2/PatientProfileController.java
+++ src/applicationV2/PatientProfileController.java
@@ -1,19 +1,41 @@
 package applicationV2;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 
 public class PatientProfileController implements Initializable{
 
+	@FXML
+	private Button mainMenuButton;
+
 	
-	
-	
-	
-	
-	
+	@FXML
+	private void handleMainMenuButton(ActionEvent e1) throws IOException {
+		Stage stage;
+		Parent root;
+		if (e1.getSource() == mainMenuButton) {
+			// finding reference for button stage
+			stage = (Stage) mainMenuButton.getScene().getWindow();
+			// now loading CreatePatientScreen as parent
+			root = FXMLLoader.load(getClass().getResource(
+					"/applicationV2/MainMenu.fxml"));
+
+			Scene scene = new Scene(root);
+			stage.setScene(scene);
+			stage.show();
+		}
+	}	
 
 	public void initialize(URL location, ResourceBundle resources) {
 		// TODO Auto-generated method stub
