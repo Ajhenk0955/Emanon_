c30274dc71c2fd07c06823448109b85ca34f6328
Actilaurel <gaticomoni@gmail.com>
2015-04-19 00:45:35 -0700

    bridges VII
    some EditUserProfile functionalities

--- src/applicationV2/EditUserProfileController.java
+++ src/applicationV2/EditUserProfileController.java
@@ -7,16 +7,24 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 public class EditUserProfileController implements Initializable {
 	
 	@FXML
 	private Button closeButton;
 
-	
+	/**
+	 * Method to close the EditUserProfile window
+	 * Author M
+	 * 
+	 */
 	@FXML
 	private void handleCloseButton(ActionEvent e){
-		
+		if(e.getSource() == closeButton){
+			Stage stage = (Stage)closeButton.getScene().getWindow();
+			   stage.close();
+		}
 	}
 	public void initialize(URL location, ResourceBundle resources) {
 		// TODO Auto-generated method stub
