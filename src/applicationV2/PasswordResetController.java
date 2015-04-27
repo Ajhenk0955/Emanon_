ebf23d9508e0098e5964977a5b045ee2684ae97c
Actilaurel <gaticomoni@gmail.com>
2015-04-26 20:14:41 -0700

    stuff I
    password reset stuff and refresh picture found for future use

--- src/applicationV2/PasswordResetController.java
+++ src/applicationV2/PasswordResetController.java
@@ -5,15 +5,19 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
 public class PasswordResetController implements Initializable {
 
 	@FXML 
-	private Button cancelButton, PasswordSubmitButton;
+	private Button cancelButton, PasswordSubmitButton, okButton;
 	
 	/**
 	 * Method to take care of the passwordSubmitButton
@@ -21,18 +25,36 @@ public class PasswordResetController implements Initializable {
 	 * @Author M
 	 * 
 	 */
-
 	@FXML
 	private void handlePasswordSubmitButton(ActionEvent e) throws IOException{
 		if(e.getSource() == PasswordSubmitButton){
 			//TODO the information gets sent back to database?
+		
+			SuccessPopup();//creates pop up "Success"
+			}
 		}
-		else{
-			Stage stage = (Stage)PasswordSubmitButton.getScene().getWindow();
-			   stage.close();
-		}
-	}
 	
+	/**
+	 * 
+	 * creates the pop up window when user successfully changes password
+	 */
+	@FXML
+	private void SuccessPopup() throws IOException{
+		Stage stage;
+		Parent root;
+		
+		stage = (Stage) PasswordSubmitButton.getScene().getWindow();
+		// now loading SearchResults as parent
+		root = FXMLLoader.load(getClass().getResource(
+				"/applicationV2/PasswordReset2.fxml"));
+
+		Scene s = new Scene(root);
+		stage.setScene(s);
+		stage.setResizable(false);
+		stage.setTitle("Emanon File System - Password Reset");
+		stage.show();	
+
+	}
 	
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		// TODO Auto-generated method stub
